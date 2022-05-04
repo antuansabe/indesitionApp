f70cb9059039631fecae64fa7566a543b03a3d38
Antonio DroCode <antuansabe@gmail.com>
2022-05-03 19:53:49 -0500

    indesitionapp setup

---
+++ bases/singleton.js
@@ -0,0 +1,44 @@
+
+
+// ES UNA INSTANCIA UNICA DE MI CLASE
+
+//Su objetivo es asegurar que existe tan
+//solo una instancia de una clase
+
+//Asi nos aseguramos que no tengamos mas instancias
+
+
+//Una forma de lo
+
+
+// Declaro una clase
+
+
+// una forma de lograr esto es haciendo privado el constructor
+//Para que asi sea inaccesible desde fuera de la clase
+//Es decir, bloqueamos que cualquiera desde fuera pueda crear instancias
+// de la misma clase.
+
+
+//Entonces, palabras mas palabras menos:
+//Lo que hace singleton es encapsular el proceso de creacion de instancias
+
+class Singleton {
+    static instancia;
+    nombre = "";
+    constructor ( nombre = " " ) {
+    
+        class Singleton {
+
+            static instancia; // Esto nos va a retornar undefined
+            nombre = "";
+    }
+
+}
+
+}
+
+
+const instancia1 = new Singleton('persona');
+
+
